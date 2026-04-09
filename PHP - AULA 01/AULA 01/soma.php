bfa7944c04cb6bc8d029eb0146417b940b1ddbd6
karlos <karlosoliveirasilva935@gmail.com>
2026-04-08 22:21:14 -0300

    PHP

--- PHP - AULA 01/AULA 01/soma.php
+++ PHP - AULA 01/AULA 01/soma.php
@@ -11,5 +11,13 @@
         <input type="number" name="num01" required/><br><br>
         <label>"Digite o 2° numero"</label><br><br>
         <input type="submit" name="num02" required/><br><br>
+        <button type="submit">Calcular</button><br><br>
     </form>
+    <?php
+    $soma = $_POST['num01']?? ''0;
+    $soma = $_POST['num02']?? ''0;
+    $soma = $_POST['num03'];
+
+    echo "<h2>A soma é: &soma</h2>"
+    ?>
 </body>
